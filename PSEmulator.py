153672c473dbf2929cb7026583c36b8c1fe8d635
Sota Abe <sota.abe@cern.ch>
2021-08-09 15:55:04 +0900

    add exec.py and optimize monitor window range

--- PSEmulator.py
+++ PSEmulator.py
@@ -108,15 +108,15 @@ class PSEmulator:
         self.time_list.append(p_time)
         self.volt_list.append(volt)
         self.curr_list.append(curr)
-        if len(self.time_list) > 40:
+        if len(self.time_list) > 60:
             del self.time_list[0]
             del self.volt_list[0]
             del self.curr_list[0]
         
-        _ax1.set_xlim(p_time - 30, p_time + 30)
+        _ax1.set_xlim(p_time - 50, p_time + 10)
         _ax1.set_ylim(-1, max(self.volt_list) + 5)
         _gr1.set_data(self.time_list, self.volt_list)
-        _ax2.set_xlim(p_time - 30, p_time + 30)
+        _ax2.set_xlim(p_time - 50, p_time + 10)
         _ax2.set_ylim(-1, max(self.curr_list) + 5)
         _gr2.set_data(self.time_list, self.curr_list)
         plt.pause(0.01)
---
+++ exec.py
@@ -0,0 +1,4 @@
+import PSEmulator as pse
+
+Emu1 = pse.PSEmulator('/dev/ttyUSB0', 9600)
+Emu1.stand_by()
